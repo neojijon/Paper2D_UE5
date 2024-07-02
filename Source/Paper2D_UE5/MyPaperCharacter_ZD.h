52c13623d1790492530fdc67ec8a40f6a3780ea0
neojijon <neojijon@gmail.com>
2024-07-02 20:20:03 +0900

    PaperCharacter클래스에  PaperZD적용함.

---
+++ Source/Paper2D_UE5/MyPaperCharacter_ZD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "MyPaperCharacter_ZD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAPER2D_UE5_API AMyPaperCharacter_ZD : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
